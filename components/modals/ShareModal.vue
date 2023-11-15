d5bcb0cb514ad748587df28fad3d688ee25e233d
Mohamed Tarek <63224925+Mohamed-Tarek-Dev@users.noreply.github.com>
2023-11-15 17:48:50 +0200

    fix

--- components/modals/ShareModal.vue
+++ components/modals/ShareModal.vue
@@ -2,12 +2,11 @@
   <div>
     <ShareNetwork
       network="facebook"
-      url="https://tender-app-ten.vercel.app/tender/4"
+      :url="`https://tender-app-ten.vercel.app/tender/${item.id}`"
       :title="this.item.title"
       :description="this.item.desc"
-      hashtags="مناقصة"
-      quote="Blah blah blah"
-      media="https://i.pinimg.com/564x/a1/34/32/a134320ae7a4b7c793a807d3207412bb.jpg"
+      :hashtags="this.item.title"
+      :quote="this.item.title"
     >
       <button>Share on Facebook</button>
     </ShareNetwork>
@@ -17,6 +16,50 @@
 <script>
 export default {
   props: ['item'],
+  head: {
+    // title: this.item.title,
+    meta: [
+      {
+        hid: 'description',
+        name: 'description',
+        content: 'Your page description goes here',
+      },
+      // Open Graph data
+      {
+        hid: 'og:title',
+        property: 'og:title',
+        content: 'Page Title',
+      },
+      {
+        hid: 'og:description',
+        property: 'og:description',
+        content: 'Your page description goes here',
+      },
+      {
+        hid: 'og:image',
+        property: 'og:image',
+        content:
+          'https://i.pinimg.com/236x/45/b6/7a/45b67a28f2d4dc0a328fc4b36d55a604.jpg',
+      },
+      // Twitter Card data
+      {
+        hid: 'twitter:title',
+        name: 'twitter:title',
+        content: 'Page Title',
+      },
+      {
+        hid: 'twitter:description',
+        name: 'twitter:description',
+        content: 'Your page description goes here',
+      },
+      {
+        hid: 'twitter:image',
+        name: 'twitter:image',
+        content:
+          'https://i.pinimg.com/236x/45/b6/7a/45b67a28f2d4dc0a328fc4b36d55a604.jpg',
+      },
+    ],
+  },
 }
 </script>
 
